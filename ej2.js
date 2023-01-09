4112a069c3f4b9d6dcaa69371d392b06baf4b098
huev0n <81804758+huev0n@users.noreply.github.com>
2023-01-09 10:54:24 +0100

    Create ej2.js

---
+++ ej2.js
@@ -0,0 +1,16 @@
+let nombre = "Pablo";
+let edad = 38;
+let desarrollador = false;
+let fecha = new Date(1984,1,13);
+let libro = {
+    titulo : "Los Pilares de la Tierra",
+    autor : "Ken Follet",
+    fecha : 1989,
+    url : 'https://es.wikipedia.org/wiki/Los_pilares_de_la_Tierra'
+}
+
+console.log(nombre);
+console.log(edad);
+console.log(desarrollador);
+console.log(fecha);
+console.log(libro);
