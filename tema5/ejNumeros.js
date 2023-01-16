465b7003b8a67dfd184af8a3a8b993c5f4d8c954
huev0n <81804758+huev0n@users.noreply.github.com>
2023-01-16 11:17:10 +0100

    Create ejNumeros.js

---
+++ tema5/ejNumeros.js
@@ -0,0 +1,6 @@
+let alturaCm = 192;
+let alturaM = 1.92;
+let pesoKg = 108.5;
+let alturaMRed = Math.ceil(alturaM);
+let pesoKgRed = Math.floor(pesoKg);
+let maxMasUno = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
